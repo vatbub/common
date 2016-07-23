5fd4acd48d5ddbde97ee43535ecd1f4766945ac4
Frederik Kammel <frederikkammel@outlook.de>
2016-07-24 00:22:35 +0200

    Added first draft of Animations

---
+++ src/main/java/common/Animations.java
@@ -0,0 +1,63 @@
+package common;
+
+import javafx.animation.ScaleTransition;
+import javafx.scene.control.Control;
+import javafx.util.Duration;
+
+public class Animations {
+
+	public static void disableControl(Control c, boolean noAnimation) {
+		if (noAnimation) {
+			disableControl(c, Duration.millis(0.1));
+		} else {
+			disableControl(c);
+		}
+	}
+
+	public static void disableControl(Control c) {
+		if (!c.isDisabled()) {
+			disableControl(c, Duration.millis(50));
+		}
+	}
+
+	private static void disableControl(Control c, Duration dur) {
+		System.out.println("Disabling control " + c.toString() + ", dur = " + dur.toMillis());
+		ScaleTransition st = new ScaleTransition(dur, c);
+		st.setByX(-0.5);
+		st.setByY(0.0);
+		st.setCycleCount(1);
+		st.setAutoReverse(false);
+
+		st.play();
+
+		c.setDisable(true);
+	}
+
+	public static void enableControl(Control c, boolean noAnimation) {
+		if (noAnimation) {
+			enableControl(c, Duration.millis(0.1));
+		} else {
+			enableControl(c);
+		}
+	}
+
+	public static void enableControl(Control c) {
+		if (!c.isDisabled()) {
+			enableControl(c, Duration.millis(50));
+		}
+	}
+
+	private static void enableControl(Control c, Duration dur) {
+		System.out.println("Enabling control " + c.toString() + ", dur = " + dur.toMillis());
+		ScaleTransition st = new ScaleTransition(dur, c);
+		st.setByX(0.5);
+		st.setByY(0.0);
+		st.setCycleCount(1);
+		st.setAutoReverse(false);
+
+		st.play();
+
+		c.setDisable(true);
+	}
+
+}
