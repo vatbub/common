d2364a8edbb06d4a854253c44509223ba1ceff37
Frederik Kammel <frederikkammel@outlook.de>
2016-07-27 01:31:33 +0200

    Version: Added support for SNAPSHOT-versions

--- src/main/java/common/Version.java
+++ src/main/java/common/Version.java
@@ -3,48 +3,63 @@ package common;
 // Code taken from http://stackoverflow.com/questions/198431/how-do-you-compare-two-version-strings-in-java
 public class Version implements Comparable<Version> {
 
-    private String version;
+	private String version;
+	private static String snapshotSuffix = "-SNAPSHOT";
 
-    @Override
-    public final String toString() {
-        return this.version;
-    }
+	@Override
+	public final String toString() {
+		return this.version;
+	}
 
-    public Version(String version) {
-        if(version == null)
-            throw new IllegalArgumentException("Version can not be null");
-        if(!version.matches("[0-9]+(\\.[0-9]+)*"))
-            throw new IllegalArgumentException("Invalid version format");
-        this.version = version;
-    }
+	public Version(String version) {
+		if (version == null)
+			throw new IllegalArgumentException("Version can not be null");
+		if (!version.matches("[0-9]+(\\.[0-9]+)*+?(" + snapshotSuffix + ")"))
+			throw new IllegalArgumentException("Invalid version format");
+		this.version = version;
+	}
 
-    @Override public int compareTo(Version that) {
-        if(that == null)
-            return 1;
-        String[] thisParts = this.toString().split("\\.");
-        String[] thatParts = that.toString().split("\\.");
-        int length = Math.max(thisParts.length, thatParts.length);
-        for(int i = 0; i < length; i++) {
-            int thisPart = i < thisParts.length ?
-                Integer.parseInt(thisParts[i]) : 0;
-            int thatPart = i < thatParts.length ?
-                Integer.parseInt(thatParts[i]) : 0;
-            if(thisPart < thatPart)
-                return -1;
-            if(thisPart > thatPart)
-                return 1;
-        }
-        return 0;
-    }
+	@Override
+	public int compareTo(Version that) {
+		if (that == null)
+			return 1;
+		String[] thisParts = this.toString().replace(snapshotSuffix, "").split("\\.");
+		boolean thisIsSnapshot = this.toString().endsWith(snapshotSuffix);
+		String[] thatParts = that.toString().replace(snapshotSuffix, "").split("\\.");
+		boolean thatIsSnapshot = that.toString().endsWith(snapshotSuffix);
+		int length = Math.max(thisParts.length, thatParts.length);
 
-    @Override public boolean equals(Object that) {
-        if(this == that)
-            return true;
-        if(that == null)
-            return false;
-        if(this.getClass() != that.getClass())
-            return false;
-        return this.compareTo((Version) that) == 0;
-    }
+		for (int i = 0; i < length; i++) {
+			int thisPart = i < thisParts.length ? Integer.parseInt(thisParts[i]) : 0;
+			int thatPart = i < thatParts.length ? Integer.parseInt(thatParts[i]) : 0;
+			if (thisPart < thatPart)
+				return -1;
+			if (thisPart > thatPart)
+				return 1;
+		}
+
+		// Version numbers are equal, check if there is a snapshot involved
+		if (thisIsSnapshot && !thatIsSnapshot) {
+			// that is a release and thus a newer version
+			return -1;
+		} else if (!thisIsSnapshot && thatIsSnapshot) {
+			// this is a release version and thus newer
+			return 1;
+		} else {
+			// both versions are equal
+			return 0;
+		}
+	}
+
+	@Override
+	public boolean equals(Object that) {
+		if (this == that)
+			return true;
+		if (that == null)
+			return false;
+		if (this.getClass() != that.getClass())
+			return false;
+		return this.compareTo((Version) that) == 0;
+	}
 
 }
