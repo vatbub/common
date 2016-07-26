d0b966057fbb07e776f3beacaa9b0254595068d2
Frederik Kammel <frederikkammel@outlook.de>
2016-07-27 01:41:48 +0200

    Version: Fixed regex as it did not match anything

--- src/main/java/common/Version.java
+++ src/main/java/common/Version.java
@@ -14,7 +14,7 @@ public class Version implements Comparable<Version> {
 	public Version(String version) {
 		if (version == null)
 			throw new IllegalArgumentException("Version can not be null");
-		if (!version.matches("[0-9]+(\\.[0-9]+)*+?(" + snapshotSuffix + ")"))
+		if (!version.matches("[0-9]+(\\.[0-9]+)*(" + snapshotSuffix + ")?"))
 			throw new IllegalArgumentException("Invalid version format");
 		this.version = version;
 	}
