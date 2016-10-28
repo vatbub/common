c7aea006d7299e7ea2422b4e610189781647ae2d
vatbub <vatbub123+github@googlemail.com>
2016-10-28 03:44:50 +0200

    Implemented Progressbar animations for [vatbub/foklauncher#25]

---
+++ src/main/java/view/CustomProgressBar.java
@@ -0,0 +1,33 @@
+package view;
+
+import javafx.animation.*;
+import javafx.scene.control.ProgressBar;
+import javafx.util.Duration;
+
+public class CustomProgressBar extends ProgressBar {
+
+	public CustomProgressBar() {
+		super();
+	}
+
+	public CustomProgressBar(double arg0) {
+		super(arg0);
+	}
+
+	/**
+	 * Like the usual {@code setProgress}-method but adds a smooth animation
+	 * 
+	 * @param progress
+	 *            The progress to set
+	 */
+	public void setProgressAnimated(double progress) {
+		Timeline timeline = new Timeline();
+
+		KeyValue keyValue = new KeyValue(this.progressProperty(), progress);
+		KeyFrame keyFrame = new KeyFrame(new Duration(500), keyValue);
+		timeline.getKeyFrames().add(keyFrame);
+
+		timeline.play();
+	}
+
+}
