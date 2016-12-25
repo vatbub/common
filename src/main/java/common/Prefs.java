66c81eb78fd9ce4804f9ae9182d267e6833ce8eb
vatbub <vatbub123+github@googlemail.com>
2016-12-25 15:02:38 +0000

    Added some JavaDoc

--- src/main/java/common/Prefs.java
+++ src/main/java/common/Prefs.java
@@ -21,6 +21,8 @@ package common;
  */
 
 
+import logging.FOKLogger;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -28,45 +30,67 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 
-import logging.FOKLogger;
-
+/**
+ * A wrapper for {@code java.util.Properties} to save preferences. The preferences are saved in the applications AppData folder, make sure to call {@link Common#setAppName(String)} prior to using this class or else you will get a {@code NullPointerException}
+ */
 public class Prefs {
-	private Properties props = new Properties();
-	private File f;
-	private static FOKLogger log = new FOKLogger(Prefs.class.getName());
+    private Properties props = new Properties();
+    private File f;
+    private static FOKLogger log = new FOKLogger(Prefs.class.getName());
 
-	public Prefs(String className) {
-		// Retrieve the user preference node for the package
-		f = new File(Common.getAndCreateAppDataPath() + className + ".properties");
+    /**
+     * Loads or creates the preference file for the specified class
+     *
+     * @param className The name of the class to load the properties file for
+     */
+    public Prefs(String className) {
+        // Retrieve the user preference node for the package
+        f = new File(Common.getAndCreateAppDataPath() + className + ".properties");
 
-		try {
-			if (f.exists()) {
-				// Load the properties
-				props.load(new FileReader(f));
-			}
-
-		} catch (IOException e) {
-			log.getLogger().log(Level.SEVERE, "An error occurred", e);
-		}
-	}
-
-	public void setPreference(String prefKey, String prefValue) {
-		props.setProperty(prefKey, prefValue);
-		savePreferences();
-	}
-
-	public String getPreference(String prefKey, String defaultValue) {
-		return props.getProperty(prefKey, defaultValue);
-	}
-	
-	public void savePreferences(){
         try {
-        	log.getLogger().info("Saving preference file as: " + f.getAbsolutePath());
-        	f.getParentFile().mkdirs();
-        	FileOutputStream out = new FileOutputStream( f );
-			props.store(out, "This is a preference file of the app " + Common.getAppName() + ". If you delete this file, the specified app will be (partly or entirely) reset to its factory settings.");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            if (f.exists()) {
+                // Load the properties
+                props.load(new FileReader(f));
+            }
+
+        } catch (IOException e) {
+            log.getLogger().log(Level.SEVERE, "An error occurred", e);
+        }
+    }
+
+    /**
+     * Sets the value of the specified preference in the properties file
+     *
+     * @param prefKey   The key of the preference to save
+     * @param prefValue The value of the preference to save
+     */
+    public void setPreference(String prefKey, String prefValue) {
+        props.setProperty(prefKey, prefValue);
+        savePreferences();
+    }
+
+    /**
+     * Returns the value of the specified preference.
+     *
+     * @param prefKey      The preference to look for
+     * @param defaultValue The value to be returned if the key was not found in the properties file
+     * @return The value of the specified preference or the {@code defaultValue} if the key was not found
+     */
+    public String getPreference(String prefKey, String defaultValue) {
+        return props.getProperty(prefKey, defaultValue);
+    }
+
+    /**
+     * Saves the properties file to the hard disk. No need to call this method explicitly as it is already called every time {@link #setPreference(String, String)} is called.
+     */
+    public void savePreferences() {
+        try {
+            log.getLogger().info("Saving preference file as: " + f.getAbsolutePath());
+            f.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(f);
+            props.store(out, "This is a preference file of the app " + Common.getAppName() + ". If you delete this file, the specified app will be (partly or entirely) reset to its factory settings.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
