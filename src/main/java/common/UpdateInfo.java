88bea4cc126044beb5786969c1f97e48f34d76dd
vatbub <vatbub123+github@googlemail.com>
2017-01-26 03:27:23 +0100

    Reformatted code

--- src/main/java/common/UpdateInfo.java
+++ src/main/java/common/UpdateInfo.java
@@ -25,48 +25,48 @@ import java.net.URL;
 
 /**
  * A class to communicate update information.
- * @see UpdateChecker
- * @author frede
  *
+ * @author frede
+ * @see UpdateChecker
  */
 public class UpdateInfo {
-	/**
-	 * Base URL of the maven repo where the artifact can be downloaded from.
-	 */
-	public URL mavenRepoBaseURL;
-	
-	/**
-	 * The artifacts group id.
-	 */
-	public String mavenGroupID;
-	
-	/**
-	 * The artifacts artifact id
-	 */
-	public String mavenArtifactID;
-	
-	/**
-	 * The artifacts classifier or {@code ""} if the default artifact shall be used.
-	 */
-	public String mavenClassifier;
-	
-	/**
-	 * Describes how the artifact is packed.
-	 */
-	public String packaging;
-	
-	/**
-	 * {@code true} if a dialog should be presented to the user to download the update
-	 */
-	public boolean showAlert;
-	
-	/**
-	 * The artifact version after the update
-	 */
-	public Version toVersion;
-	
-	/**
-	 * The file size of the artifact in Megabytes. If the file size could not be determined, the value of this field is {@code -1}.
-	 */
-	public double fileSizeInMB;
+    /**
+     * Base URL of the maven repo where the artifact can be downloaded from.
+     */
+    public URL mavenRepoBaseURL;
+
+    /**
+     * The artifacts group id.
+     */
+    public String mavenGroupID;
+
+    /**
+     * The artifacts artifact id
+     */
+    public String mavenArtifactID;
+
+    /**
+     * The artifacts classifier or {@code ""} if the default artifact shall be used.
+     */
+    public String mavenClassifier;
+
+    /**
+     * Describes how the artifact is packed.
+     */
+    public String packaging;
+
+    /**
+     * {@code true} if a dialog should be presented to the user to download the update
+     */
+    public boolean showAlert;
+
+    /**
+     * The artifact version after the update
+     */
+    public Version toVersion;
+
+    /**
+     * The file size of the artifact in Megabytes. If the file size could not be determined, the value of this field is {@code -1}.
+     */
+    public double fileSizeInMB;
 }
