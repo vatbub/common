6d2e7e5668057bb7aa774535784c457f672ac4ee
vatbub <vatbub123+github@googlemail.com>
2017-02-01 04:25:07 +0100

    Modified the screenshot file name on aws again

--- src/main/java/view/reporting/ReportingDialog.java
+++ src/main/java/view/reporting/ReportingDialog.java
@@ -344,7 +344,7 @@ public class ReportingDialog {
                     // upload the screenshot to aws
                     Platform.runLater(() -> ReportingDialogUploadProgress.getStatusLabel().setText(bundle.getString("uploadingScreenshot")));
 
-                    String awsFileName = Common.getAppName() + "/screenshots/screenshot" + Common.getLaunchTimeStamp() + ".png";
+                    String awsFileName = Common.getAppName() + "/screenshots/screenshot_" + Common.getAppName() + "_" + Common.getLaunchTimeStamp() + ".png";
                     gitHubIssue.setScreenshotLocation(awsFileName);
 
                     // upload the logs to s3
