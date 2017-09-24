9368f4547bb935e14b2ee526740230dd3e805bc7
vatbub <vatbub123+github@googlemail.com>
2017-09-24 03:07:33 +0200

    Made LabelUtils.computeTextWidth public

--- view/core/src/main/java/com/github/vatbub/common/view/core/LabelUtils.java
+++ view/core/src/main/java/com/github/vatbub/common/view/core/LabelUtils.java
@@ -1,5 +1,26 @@
 package com.github.vatbub.common.view.core;
 
+/*-
+ * #%L
+ * FOKProjects Common View Core
+ * %%
+ * Copyright (C) 2016 - 2017 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import com.sun.javafx.scene.text.TextLayout;
 import com.sun.javafx.tk.Toolkit;
 import javafx.geometry.Bounds;
@@ -197,7 +218,7 @@ public class LabelUtils {
         }
     }
 
-    static double computeTextWidth(Font font, String text, double wrappingWidth) {
+    public static double computeTextWidth(Font font, String text, double wrappingWidth) {
         //noinspection deprecation
         layout.setContent(text != null ? text : "", font.impl_getNativeFont());
         layout.setWrapWidth((float) wrappingWidth);
