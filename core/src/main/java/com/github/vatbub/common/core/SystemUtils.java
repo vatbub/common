cdccc38941e68e4f8ab0aff963d65ce2c072be81
Frederik Kammel <vatbub123+github@googlemail.com>
2018-05-03 15:56:56 +0200

    Added new SystemUtils class with a shutdown method and a shutdown daemon

---
+++ core/src/main/java/com/github/vatbub/common/core/SystemUtils.java
@@ -0,0 +1,94 @@
+package com.github.vatbub.common.core;
+
+/*-
+ * #%L
+ * FOKProjects Common Core
+ * %%
+ * Copyright (C) 2016 - 2018 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.github.vatbub.common.core.logging.FOKLogger;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+public class SystemUtils {
+    private static SystemUtils instance;
+    private Timer timer;
+
+    private SystemUtils() {
+    }
+
+    public static SystemUtils getInstance() {
+        if (instance == null)
+            instance = new SystemUtils();
+        return instance;
+    }
+
+    public boolean shutComputerDown(int time) throws IOException {
+        String shutdownCommand, t = time == 0 ? "now" : String.valueOf(time);
+
+        if (org.apache.commons.lang3.SystemUtils.IS_OS_AIX)
+            shutdownCommand = "shutdown -Fh " + t;
+        else if (org.apache.commons.lang3.SystemUtils.IS_OS_FREE_BSD || org.apache.commons.lang3.SystemUtils.IS_OS_LINUX || org.apache.commons.lang3.SystemUtils.IS_OS_MAC || org.apache.commons.lang3.SystemUtils.IS_OS_MAC_OSX || org.apache.commons.lang3.SystemUtils.IS_OS_NET_BSD || org.apache.commons.lang3.SystemUtils.IS_OS_OPEN_BSD || org.apache.commons.lang3.SystemUtils.IS_OS_UNIX)
+            shutdownCommand = "shutdown -h " + t;
+        else if (org.apache.commons.lang3.SystemUtils.IS_OS_HP_UX)
+            shutdownCommand = "shutdown -hy " + t;
+        else if (org.apache.commons.lang3.SystemUtils.IS_OS_IRIX)
+            shutdownCommand = "shutdown -y -g " + t;
+        else if (org.apache.commons.lang3.SystemUtils.IS_OS_SOLARIS || org.apache.commons.lang3.SystemUtils.IS_OS_SUN_OS)
+            shutdownCommand = "shutdown -y -i5 -g" + t;
+        else if (org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS_XP || org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS_VISTA || org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS_7)
+            shutdownCommand = "shutdown.exe -s -t " + t;
+        else
+            return false;
+
+        Runtime.getRuntime().exec(shutdownCommand);
+        return true;
+    }
+
+    public void startAutoShutdownDaemon(Callable<Boolean> predicate, long predicateCheckPeriodDuration, TimeUnit predicateCheckPeriodUnit) {
+        if (timer != null)
+            timer.cancel();
+        timer = new Timer("AutoShutdownDaemon");
+        Date executionDate = new Date(); // no params = now
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    if (predicate.call()) {
+                        boolean res = shutComputerDown(0);
+                        if (!res)
+                            FOKLogger.log(getClass().getName(), Level.SEVERE, "Unable to shut computer down: shutComputerDown returned false");
+                    }
+                } catch (Exception e) {
+                    FOKLogger.log(getClass().getName(), Level.SEVERE, "Unable to shut computer down" + e);
+                }
+            }
+        }, executionDate, TimeUnit.MILLISECONDS.convert(predicateCheckPeriodDuration, predicateCheckPeriodUnit));
+    }
+
+    public void cancelAutoShutdownDaemon() {
+        if (timer != null)
+            timer.cancel();
+    }
+}
